e861866ecfef98cf944688e6187c8ec7f032710d
Leonardo <lchavesbr@gmail.com>
2025-08-14 23:57:22 -0300

    Correct new line after entering input value.

--- homeworks/homework_2/fortune_teller/fortune_teller.cpp
+++ homeworks/homework_2/fortune_teller/fortune_teller.cpp
@@ -12,17 +12,19 @@ int main(){
     cout << "Please enter your name:" << endl;
     std::string name{};
     cin >> name;
+    cout << endl;
     cout << "Please enter the time of year when you were born:" << endl;
     cout << "(pick from 'spring', 'summer', 'autumn', 'winter')" << endl;
     std::string season{};
     cin >> season;
-
+    cout << endl;
     std::array<std::string,2> adjective{};
     cout << "Please enter an adjective:";
     cin >> adjective.at(0);
+    cout << endl;
     cout << "Please enter another adjective:";
     cin >> adjective.at(1);
-
+    cout << endl;
     std::map<std::string, std::string> const noun{{"spring","STL guru"},
                                                   {"summer","C++ expert"},
                                                   {"autumn","coding beast"},
