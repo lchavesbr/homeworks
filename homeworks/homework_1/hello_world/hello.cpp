df7d82a422b20e9274a3daef6472b4264e26f6cd
Leonardo <lchavesbr@gmail.com>
2025-08-14 23:45:23 -0300

    Delete old and unstaged HW 1 and add Homework 2.

--- homeworks/homework_1/hello_world/hello.cpp
+++ homeworks/homework_1/hello_world/hello.cpp
@@ -1,5 +1,5 @@
 #include <cstdio>
 int main(){
-    std::printf("Hello World\n");
+    std::printf("Hello World!\n");
     return 0;
 }
---
+++ homeworks/homework_2/fortune_teller/fortune_teller.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <array>
+#include <vector>
+#include <map>
+
+int main(){
+    using std::cout;
+    using std::endl;
+    using std::cin;
+
+    cout << "Welcome to the fortune teller program!" << endl;
+    cout << "Please enter your name:" << endl;
+    std::string name{};
+    cin >> name;
+    cout << "Please enter the time of year when you were born:" << endl;
+    cout << "(pick from 'spring', 'summer', 'autumn', 'winter')" << endl;
+    std::string season{};
+    cin >> season;
+
+    std::array<std::string,2> adjective{};
+    cout << "Please enter an adjective:";
+    cin >> adjective.at(0);
+    cout << "Please enter another adjective:";
+    cin >> adjective.at(1);
+
+    std::map<std::string, std::string> const noun{{"spring","STL guru"},
+                                                  {"summer","C++ expert"},
+                                                  {"autumn","coding beast"},
+                                                  {"winter","software design hero"}
+                                                };
+    std::vector<std::string> const ending{"eats UB for breakfast",
+                                    "finds errors quicker than the compiler",
+                                    "is not afraid of C++ error messages"};
+    cout << "Here is your description:\n";
+    cout << name + ", the " << adjective.at(name.size()%adjective.size()) << " " << noun.at(season) << " that " << ending.at(name.size()%ending.size()) << endl;
+
+    return 0;
+}
